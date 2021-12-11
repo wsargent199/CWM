b88e19242d405ec5f215a321df5f7cdec803f71f
wsargent199 <95759018+wsargent199@users.noreply.github.com>
2021-12-11 17:53:16 -0500

    Update circles.py

--- circles.py
+++ circles.py
@@ -1,33 +1,54 @@
-# import the necessary packages
+from zipfile import ZipFile
+from os import path
+from os.path import basename
+from scipy.spatial import distance as dist
+#from imutils import perspective
+#from imutils import contours
+
+fromaddr = "cwm.sn.1021@gmail.com"
+toaddr = "sargentw@gmail.com"
+password = "digilube1021"
+chain_name = "chainxxx"
+
+
 import numpy as np
-#import argparse
 import cv2
+import socket
+import urllib.request
 
-# construct the argument parser and parse the arguments
-#ap = argparse.ArgumentParser()
-#ap.add_argument("-i", "--image", required = True, help = "Path to the image")
-#args = vars(ap.parse_args())
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
-# load the image, clone it for output, and then convert it to grayscale
-image = cv2.imread(args["image"])
-output = image.copy()
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = chain_name
 
-# detect circles in the image
-circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
 
-# ensure at least some circles were found
-if circles is not None:
-	# convert the (x, y) coordinates and radius of the circles to integers
-	circles = np.round(circles[0, :]).astype("int")
+body = "Chain_Name: "
+buf = chain_name
+body = body + buf
 
-	# loop over the (x, y) coordinates and radius of the circles
-	for (x, y, r) in circles:
-		# draw the circle in the output image, then draw a rectangle
-		# corresponding to the center of the circle
-		cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-		cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
 
-	# show the output image
-	cv2.imshow("output", np.hstack([image, output]))
-	cv2.waitKey(0)
+
+
+msg.attach(MIMEText(body, 'plain'))
+
+filename = "/home/pi/CWM_DATA/cfg.txt"
+attachment = open("/home/pi/CWM_DATA/cfg.txt", "rb")
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+msg.attach(part)
+
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, "digilube1021")
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
