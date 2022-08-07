dd1b6298d2fd2400b989e2e0ad8267e62137e26b
wsargent199 <95759018+wsargent199@users.noreply.github.com>
2022-08-07 19:02:47 -0400

    Add files via upload

---
+++ move_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /home/pi/CWM
+echo "moving cfg.txt"
+cp cfg.txt /home/pi/CWM_DATA/cfg.txt
+echo "moving measurexGS.py"
+cp measurexGS.py /home/pi/mu_code/measurexGS.py
+echo "moving measurexGS.py"
+cp measureGS.py /home/pi/mu_code/measureGS.py
+echo "moving main.cpp"
+cp cfg.txt /home/pi/CWM_DATA/cfg.txt
+cd ..
+echo "goodbye"
