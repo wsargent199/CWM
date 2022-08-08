aae0fc6a42796ad6daf8172d6476f08f121d7c1b
wsargent199 <95759018+wsargent199@users.noreply.github.com>
2022-08-08 08:58:22 -0400

    Add files via upload

--- measureGS.py
+++ measureGS.py
@@ -78,6 +78,7 @@ off_cycles   = 1
 last_pin_rd  = 0
 
 hack_offset = 0;
+first_cycle = 1;
 
 
 
@@ -97,7 +98,7 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 800)
 cap.set(38,3)     # would love to set buffsize to 1 ,,  but 3 is as low as it goes ???
-os.system("v4l2-ctl -c exposure=400")               # exposure values min=006 max=906 default=800    higher number = longer exposure  doi!
+os.system("v4l2-ctl -c exposure=200")               # exposure values min=006 max=906 default=800    higher number = longer exposure  doi!
 #millisec1 = int(round(time.time() * 1000))   # take time snapshot
 #print (" VideoCap init complete ",(millisec1 - millisec))
 
@@ -108,7 +109,6 @@ MyDateTime = datetime.datetime(2016,1,3,8,30,20)
 #honda_FN  = "asdfghjkl"
 
 counter=0
-first_cycle = 0
 
 
 
@@ -189,8 +189,8 @@ now = datetime.datetime.now()
 buf = (now.strftime("%Y-%m-%d %H:%M:%S"))
 
 # load default values in case there is no cfg.csv file   ** and create the variable ..  maybe nesc / maybe not,  python confuses me
-chain_name = "TP2"
-BaseLine_File_Name = "TP2_baseline.csv"
+chain_name = "Heat Treat Line1"
+BaseLine_File_Name = "baseline.csv"
 output_file_name = "resultx.csv"
 output_file_type = "delta"
 stretch_limit = "0.0125"
@@ -200,7 +200,7 @@ this_baseline_len = 2.500
 chain_direction = 'rtl'
 send_email = 'yes'
 fromaddr = "cwm.sn.1021@gmail.com"
-toaddr = "sargentw@gmail.com;wsargent199@yahoo.com"
+toaddr = "sargentw@gmail.com"
 password = "digilube1021"
 off_cycles_cfg = "1000"
 downstream = "10"
@@ -212,7 +212,6 @@ GPIO.setwarnings(False)
 GPIO.setup(40,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(38,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(36,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(12, GPIO.OUT)
@@ -375,6 +374,7 @@ with open("/home/pi/CWM_DATA/cfg.txt", 'r') as reader:
     if (debug == 1):
         print("password > ",password)
         
+        
     #read the off cycles line
     buf10 = reader.readline()                      # read entire line
     off_cycles_cfg = buf10[21:(len(buf10)-1)]      # cut out just the stretch limit part
@@ -389,7 +389,9 @@ with open("/home/pi/CWM_DATA/cfg.txt", 'r') as reader:
     this_downstream = int(downstream)
 
     if (debug == 1):
-        print("downstream > ",this_downstream)     
+        print("downstream > ",this_downstream)       
+        
+
 
 #filenamex = "/media/pi/" + thumb_name_pure + "/CWM/"     #results_%d.csv" % (sequence)
 filenamex = "/home/pi/CWM_DATA"
@@ -517,8 +519,7 @@ while(True):
     
     
     print ("OFF CYCLES = ", off_cycles)
-    ser.write(bytes("xxx3\r",'UTF-8'))    
-
+ 
     if off_cycles > 0:
         if GPIO.input(11):
             GPIO.output(11,GPIO.LOW)
@@ -527,7 +528,6 @@ while(True):
                 last_pin_rd = 1
         else:
             last_pin_rd = 0;
-
     #length_o_address=len(a_string)
     #print(length_o_address)
     
@@ -550,14 +550,16 @@ while(True):
         if word.isdigit():
             numbers.append(int(word))
             w = int(word)
-
+    if w==1:
+        ser.write(bytes("xxx3\r",'UTF-8'))    
+        
     if w == 1:
         if last_w > 580 and last_w < 585:
             hack_offset = last_w
         else:
             hack_offset = 0
 
-    w = w + hack_offset
+    #w = w + hack_offset
 
 
 
@@ -583,12 +585,13 @@ while(True):
             survey_state = 3            # if we have just finished a survey then send the stuff 
         
         if(survey_state == 0):
-            
+    
+            off_cycles += 1
             if (first_cycle == 1):
                 first_cycle = 0
-            else:
-                off_cycles += 1
-            if (off_cycles > this_off_cycle):         #    60  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+                off_cycles = 1
+            
+            if (off_cycles > this_off_cycle):	#*********************************************************** 200*
                 off_cycles = 0
                 survey_state = 2        # go straight to survey in progress  ( armed reserved for dashboard frc survey button )
             
@@ -598,6 +601,9 @@ while(True):
         print ("survey_state = ", (survey_state))
         print ("  off_cycles = ", (off_cycles))
         
+        test_payload = "*g%06d" % (off_cycles)
+        os.write(pipe_fifo,test_payload.encode())
+    
         
         #if (sequence == 0):
         #    saved_TS = (now.strftime("%Y-%m-%d %H:%M:%S"))
@@ -638,12 +644,10 @@ while(True):
 
             if(survey_state == 3):  # if survey just completed then prepare and send the data 
                 
-                
+                off_cycles = 1
                 test_payload = "*a%06d" % (777777)
                 os.write(pipe_fifo,test_payload.encode())
                 GPIO.output(12, GPIO.LOW)
-                off_cycles = 1
-            
                 length_in = 0.001
                 buf = "%1.3f\r\n" % (length_in)
                 while file_rec_count < 10000:
@@ -652,12 +656,12 @@ while(True):
                 fcsv.close()
 
 
-                #zipFilesInDir('/home/pi/CWM_DATA/', zipfilename, lambda name : 'png' in name)
-                zipFilesInDir('/mnt/ramdisk/', zipfilename, lambda name : 'png' in name)
+                zipFilesInDir('/home/pi/CWM_DATA/', zipfilename, lambda name : 'png' in name)
+                #zipFilesInDir('/mnt/ramdisk/', zipfilename, lambda name : 'png' in name)
                 
                 # Get a list of all the file paths that ends with .png in aspecified directory
-                #fileList = glob.glob('/home/pi/CWM_DATA/*.png')
-                fileList = glob.glob('/mnt/ramdisk/*.png')
+                fileList = glob.glob('/home/pi/CWM_DATA/*.png')
+                #fileList = glob.glob('/mnt/ramdisk/*.png')
 
                 # Iterate over the list of filepaths & remove each file.
                 for filePath in fileList:
@@ -668,8 +672,8 @@ while(True):
                         nothing = 1
 
                 # Get a list of all the file paths that ends with .jpg in aspecified directory
-                #fileList = glob.glob('/home/pi/CWM_DATA/*.jpg')
-                fileList = glob.glob('/mnt/ramdisk/*.jpg')
+                fileList = glob.glob('/home/pi/CWM_DATA/*.jpg')
+                #fileList = glob.glob('/mnt/ramdisk/*.jpg')
                 # Iterate over the list of filepaths & remove each file.
                 for filePath in fileList:
                     try:
@@ -677,9 +681,7 @@ while(True):
                     except:
                         #print("Error while deleting file : ", filePath)
                         nothing = 1
-
-                survey_state = 0
-                off_cycles = 1
+                
 
                 if send_email == "yes":
                     msg = MIMEMultipart()
@@ -725,8 +727,8 @@ while(True):
                     part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
                     msg.attach(part)
 
-                    filename = destz                 #destz #"/home/pi/CWM_DATA/cfg.txt"  *or*   = destz
-                    attachment = open(destz,"rb")    #("/home/pi/mu_code/measureGS.py", "rb")      #destz
+                    filename = destz   #destz   #"/home/pi/CWM_DATA/cfg.txt"  *or*   = destz
+                    attachment = open(destz, "rb")      #destz
                     part = MIMEBase('application', 'octet-stream')
                     part.set_payload((attachment).read())
                     encoders.encode_base64(part)
@@ -737,12 +739,7 @@ while(True):
                     server.starttls()
                     server.login(fromaddr, password)
                     text = msg.as_string()
-                    print(toaddr)
-                    recipients = ['sargentw@gmail.com','wsargent199@yahoo.com']
-                    print(recipients)
-                    #toaddr = recipients
-                    print(toaddr)
-                    server.sendmail(fromaddr, recipients, text)
+                    server.sendmail(fromaddr, toaddr, text)
                     server.quit()
 
                 if send_email == "no":
@@ -813,8 +810,8 @@ while(True):
 
    
             #filenamex = "/media/pi/" + thumb_name_pure + "/CWM/"
-            #filenamex = "/home/pi/CWM_DATA/"
-            filenamex = "/mnt/ramdisk/"
+            filenamex = "/home/pi/CWM_DATA/"
+            #filenamex = "/mnt/ramdisk/"
             filenamey = str(output_file_name)
             filenamez = "_%d.csv" % (sequence)
             filename = filenamex + filenamey + filenamez
@@ -856,26 +853,28 @@ while(True):
         zed = zed+1
         gray = cv2.GaussianBlur(frame, (11,11), 0)
 
-        #lines = ("/home/pi/CWM_DATA/box%d.jpg" % (w))
-        lines = ("/mnt/ramdisk/box%d.jpg" % (w))
+        lines = ("/home/pi/CWM_DATA/box%d.jpg" % (w))
+        #lines = ("/mnt/ramdisk/box%d.jpg" % (w))
         new_frame = 2   # voodoo
 
 
         if (new_frame==2):
             new_frame=0
 
-            post_process_FN = filename = ("/mnt/ramdisk/boo2_%d.png"  %(w))
+            #post_process_FN = filename = ("/mnt/ramdisk/boo2_%d.png"  %(w))
+            post_process_FN = filename = ("/home/pi/CWM_DATA/boo2_%d.png"  %(w))
 
             w = w + 1
 
             millisec1 = int(round(time.time() * 1000))   # take time snapshot
             print ("converted to gray",(millisec1 - millisec))
 
-            #(thresh, blackAndWhiteImage) = cv2.threshold(gray, 225, 255, cv2.THRESH_BINARY)
-
+            
             #flipped = cv2.flip(gray,0)
             #cv2.imwrite((lines),flipped)
+            
             cv2.imwrite((lines),gray)
+            
             millisec1 = int(round(time.time() * 1000))   # take time snapshot
             print ("converted to b+w",(millisec1 - millisec))
             #cv2.imshow('Before blur', frame)
@@ -979,12 +978,9 @@ while(True):
                     
             state = 0
             sub_state = 0
-            #idx_x = 639
+            idx_x = 639
             idx_y = 790
-            if (1):
-                idx_x = 639
-            else:
-                idx_x = 5
+            #idx_x = 5
             millisec1 = int(round(time.time() * 1000)) 
             print ("donehlf",(millisec1 - millisec))
             
@@ -1087,7 +1083,19 @@ while(True):
             print ("donea",(millisec1 - millisec))
 
             lth = last_good_rt_scan -last_good_lft_scan
-            length_in = lth * .0089
+            length_in = lth * .00829
+
+            if length_in > 3.6:
+                length_in = 2.6000
+            if length_in < 2.7:
+                length_in = 2.6000
+            if y_left > y_right:
+                if (y_left - y_right) > 30:
+                    length_in = 2.6000
+            if y_right > y_left:
+                if (y_right - y_left) > 30:
+                    length_in = 2.6000
+
             buf = "% 1.3f inch" % (length_in)
             #print (buf)
 
@@ -1097,7 +1105,7 @@ while(True):
             #    file_rec_count = file_rec_count+1
 
 
-            draw.line((90,90,350,90), fill = 0, width = 150)   #in from right , down from top
+            draw.line((90,90,350,90), fill = 0, width = 185)   #in from right , down from top
 
 
             #draw.text((last_good_lft_scan+10,y_left+10), buf, fill = (0))
@@ -1127,6 +1135,7 @@ while(True):
 
             crap = baseline_data[w-1]
             this_baseline_len = float(crap)
+            this_baseline_len = 2.51
             buf = "Baseline Link Length % 1.3fin" % (this_baseline_len)
             ImageDraw.Draw(r).text((10,85), buf, font = font , fill = (255))
 
@@ -1143,7 +1152,7 @@ while(True):
 
             if output_file_type == "absolute" :
                 if length_in > this_stretch_limit :
-                    buf = "Alarm = True"
+                    buf = "Alarm = False"
                     mark_this_link = "true"
                     alarm_appender = "link %d     over size limit   length = %1.3f in\r" %((w-1),length_in)
                     alarm_list = alarm_list + alarm_appender
@@ -1159,6 +1168,7 @@ while(True):
 
 
             r.save(post_process_FN)
+            #r.save("/home/pi/mu_code/pp_img.png")
 
             #last_w = w
 
@@ -1174,24 +1184,26 @@ while(True):
                     fcsv.write(buf1)
                     file_rec_count = file_rec_count+1
 
-            #imagex = cv2.imread(post_process_FN,0)
+            imagex = cv2.imread(post_process_FN,0)
             #cv2.imshow('frame', imagex)
-            #cv2.waitKey(1000)
+            #cv2.waitKey(10)
 
             millisec1 = int(round(time.time() * 1000)) 
             print ("done2",(millisec1 - millisec))
             ser.write(bytes("!\r",'UTF-8'))
-            if mark_this_link == "true" :
+            if mark_this_link == "true" :   
                 ser.write(bytes("link processed ALARM\r",'UTF-8'))
             else:
                 ser.write(bytes(" link processed OK  \r",'UTF-8'))
 
             if mark_this_link == "true" :
                     if paint_enable == "yes" :
-                        ser.write(bytes("xxx3\r",'UTF-8'))
-            else:
-                    if paint_enable == "yes" :
-                        ser.write(bytes("yyy3\r",'UTF-8'))
+                        print( "sening paint command" )
+                        #ser.write(bytes("xxx3\r",'UTF-8'))
+            #else:
+                    #if paint_enable == "yes" :
+                        #print( "sening paint command" )
+                        #ser.write(bytes("yyy3\r",'UTF-8'))
 
 # When everything done, release the capture
 GPIO.output(18, GPIO.LOW)
